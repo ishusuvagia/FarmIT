014082b96a3ea432a1cc4f06fa38e2999dd95e2e
FarmIT iOS Application <ishusuvagia@gmail.com>
2016-08-02 10:50:33 +0530

    FarmIT Update
    All File Updated New

---
+++ FarmIT App/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  FarmIT App
+//
+//  Created by iSquareOne on 01/04/15.
+//  Copyright (c) 2015 iSquareOne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
